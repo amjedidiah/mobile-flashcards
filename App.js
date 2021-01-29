8b21881710ca573ce4b92f668ed7b6c2e93c4668
amjedidiah <imunacode@gmail.com>
2021-01-29 14:57:29 +0100

    chore: Complete project setup

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
-import * as React from "react";
-import { View, Text } from "react-native";
+import * as React from 'react';
+import {View, Text} from 'react-native';
 
+/**
+ * App component
+ * @return {object}
+ */
 export default function App() {
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        justifyContent: 'center',
+        alignItems: 'center',
       }}
     >
       <Text>Universal React with Expo</Text>
