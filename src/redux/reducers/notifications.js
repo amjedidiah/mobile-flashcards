b2d1f2ac00d51bb4838707190d2d3a53fb534dee
amjedidiah <imunacode@gmail.com>
2021-01-30 13:39:27 +0100

    feat: Notification and cleanup

--- src/redux/reducers/notifications.js
+++ src/redux/reducers/notifications.js
@@ -4,10 +4,7 @@
  * @param {action} action
  * @return {boolean}
  */
-const notifications = (
-    state = null,
-    {type, notification},
-) =>
+const notifications = (state = null, {type, notification}) =>
   ({
     SAVE_DECK_PROGRESS: false,
     SAVE_NOTIFICATION: notification,
